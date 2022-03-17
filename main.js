6ac87651405936a05e7f3d8bd842b6d4daffb723
KimJeBu1 <95723051+KimJeBu1@users.noreply.github.com>
2022-03-17 17:59:09 +0900

    Game Start by Ellie

--- main.js
+++ main.js
@@ -1,14 +1,58 @@
 'use strict'
 
 const  CARROT_SIZE = 80;
+const CARROT_COUNT = 5;
+const BUG_COUNT = 5
 const field = document.querySelector('.game__field');
 const fieldRect = field.getBoundingClientRect();
+const gameBtn = document.querySelector('.game__button'); 
+const gameTimer = document.querySelector('.game__timer');
+const gameScore = document.querySelector('.game__score');
+
+let started = false; //게임이 시작되었는지 확인 하는 변수
+let score = 0;
+let timer = undefined;
+
+
+
+gameBtn.addEventListener('click', () => {
+    if(started) {
+        stopGame();
+    } else {
+        startGame()
+    }
+    started = !started; //started의 반대 boolen이 할당
+});
+
+function startGame() {
+    initGame();
+    showStopButton();
+    showTimerAndScore();
+    startGameTimer();
+}
+
+function stopGame() {
+
+}
+
+function showStopButton(){
+    const icon = gameBtn.querySelector('.fa-play');
+    icon.classList.add('fa-stop');
+    icon.classList.remove('fa-play');
+};
+
+function showTimerAndScore() {
+    gameTimer.style.visibility = 'visible';
+    gameScore.style.visibility = 'visible';
+}
 
 function initGame() {
+    field.innerHTML = '';
+    gameScore.innerText = CARROT_COUNT;
     //벌레와 당근을 생성한뒤 field에 추가해줌
     console.log(fieldRect);
-    addItem('carrot', 5, 'img/carrot.png');
-    addItem('bug', 5, 'img/bug.png')
+    addItem('carrot', CARROT_COUNT, 'img/carrot.png');
+    addItem('bug', BUG_COUNT, 'img/bug.png')
 }
 
 function addItem(className, count, imgPath) {
@@ -33,4 +77,9 @@ function randomNumber(min, max) {
     return Math.random() * (max - min) + min;
 }
 
-initGame();
+// initGame();
+
+// StartBtn.addEventListener('click', () =>{
+//     field.style.opacity = '1';
+// })
+
