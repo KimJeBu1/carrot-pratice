b3acd8eb87a08ed781aec18541bf611fb5d5f52a
KimJeBu1 <95723051+KimJeBu1@users.noreply.github.com>
2022-04-18 15:37:49 +0900

    builder pattern add in game.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 'use strict';
 
 import PopUp from './popup.js';
-import Game from './game.js';
+import GameBuilder from './game.js';
 
 const gameFinishBanner = new PopUp();
-const game = new Game(5, 5, 5);
+const game = new GameBuilder()
+.withGameDuration(5)
+.withcarrotCount(3)
+.withbugCount(3)
+.build()
 game.setGameStopListener((reason)=> {
   console.log(reason);
   let message;
