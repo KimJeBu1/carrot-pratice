c3747f110dbd05b4e8897e9881188b3fcabd5aea
KimJeBu1 <95723051+KimJeBu1@users.noreply.github.com>
2022-04-18 16:02:27 +0900

    reason object add in game.js

--- src/game.js
+++ src/game.js
@@ -3,8 +3,14 @@
 import Field from './field.js';
 import * as sound from './sound.js';
 
+export const Reason = Object.freeze({
+  win: 'win',
+  lose: 'lose',
+  cancel: 'cancel',
+});
+
 //Builder Pattern
-export default class GameBuilder {
+export class GameBuilder {
   withGameDuration(duration) {
     this.gameDuration = duration;
     return this; //class자체를 return
@@ -74,7 +80,7 @@ class Game {
       this.hideGameButton();
       sound.playAlert();
       sound.stopBackground();
-      this.onGameStop && this.onGameStop('cancel');
+      this.onGameStop && this.onGameStop(Reason.cancel);
     }
 
     finish(win) {
@@ -87,7 +93,7 @@ class Game {
       }
       this.stopGameTimer();
       sound.stopBackground();
-      this.onGameStop && this.onGameStop(win ? 'win' : 'lose');
+      this.onGameStop && this.onGameStop(win ? Reason.win : Reason.lose);
     }
 
     onItemClick = (item) => {
